f77d6d27092162758e207d5e4cb5f165217cf6b9
Кузнецов Станислав <45289597+ldontunderstand0@users.noreply.github.com>
2023-12-19 21:02:38 +0300

    Update util.h

--- lab1/util.h
+++ lab1/util.h
@@ -7,7 +7,4 @@
 // Разбиение строки на токены
 void tokenize(char* input, char* tokens[], int* token_count);
 
-// Компаратор для имен файлов и папок
-int compare_filenames(const void* a, const void* b);
-
 #endif
