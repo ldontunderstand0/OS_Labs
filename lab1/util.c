2086f3533b5602c588af68c6263c74c83d1d41ce
Кузнецов Станислав <45289597+ldontunderstand0@users.noreply.github.com>
2023-12-19 21:02:19 +0300

    Update util.c

--- lab1/util.c
+++ lab1/util.c
@@ -11,10 +11,3 @@ void tokenize(char* input, char* tokens[], int* token_count) {
     
     tokens[*token_count] = NULL;
 }
-
-int compare_filenames(const void* a, const void* b) {
-    const char* filename1 = *(const char**)a;
-    const char* filename2 = *(const char**)b;
-
-    return strcmp(filename1, filename2);
-}
